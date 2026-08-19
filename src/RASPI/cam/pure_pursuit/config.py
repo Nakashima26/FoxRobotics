12dff410539bc6802f0fdea2328bf124fea62c21
Erick Blanco Nakashima <Erick.blanco@cetysuniversidad.edu.mx>
2026-08-19 13:01:34 -0700

    Update config.py

--- src/RASPI/cam/pure_pursuit/config.py
+++ src/RASPI/cam/pure_pursuit/config.py
@@ -85,8 +85,8 @@ FLOOR_UPPER_BLUE_WIDE = np.array([150, 255, 230])
 
 WALL_STRUCTURE_PX = 15   # <-- ajusta viendo tu imagen BEV real
 
-FLOOR_LOWER_ORANGE = np.array([0, 50, 10])
-FLOOR_UPPER_ORANGE = np.array([35, 255, 245])
+FLOOR_LOWER_ORANGE = np.array([12, 110, 80])
+FLOOR_UPPER_ORANGE = np.array([30, 255, 255])
 
 # Lista usada por centerline.py — agrega más tuplas aquí si detectas más
 # colores de piso que no quieres que bloqueen la ruta.
