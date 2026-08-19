32eda2abe67503049d5bdb25565d41b67083e1e4
Erick Blanco Nakashima <Erick.blanco@cetysuniversidad.edu.mx>
2026-08-19 15:28:33 -0700

    Update PurePursuit.ino

--- src/ESP32/PurePursuit/PurePursuit.ino
+++ src/ESP32/PurePursuit/PurePursuit.ino
@@ -126,7 +126,7 @@ const unsigned long recuperandoTimeoutMs = 1500;
 // ── Giros ─────────────────────────────────────────────────────────────────────
 bool direccionIzquierda = true;
 bool primerGiro         = false;
-int  AngGiro            =82;
+int  AngGiro            =88;
 unsigned long lastTurnTime = 0;
 int timeStart = 0;
 const int cooldownGiro     = 1500;   // ms entre giros
